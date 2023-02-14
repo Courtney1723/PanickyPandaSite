a10d0d5390ad1770c030ae285807bdc9c61b806e
Courtney1723 <92752814+Courtney1723@users.noreply.github.com>
2023-02-14 12:32:30 -0700

    Fixed minor navigational bug

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Navbar />
       <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="Emojis" element={<Emojis />}/>
+          <Route path="/Emojis" element={<Emojis />}/>
           <Route path="*" element={<NoMatch />} />
       </Routes>
     </div>
